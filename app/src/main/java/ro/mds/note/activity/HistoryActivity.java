62a9f719c14918236be0faa1dbc42e462662f1f2
kkrogmerio <aditaavram@yahoo.com>
2020-04-21 22:06:49 +0100

    Merge branch 'master' of C:\Users\PC HOME\Documents\GitHub\mds-note with conflicts.

--- app/src/main/java/ro/mds/note/activity/HistoryActivity.java
+++ app/src/main/java/ro/mds/note/activity/HistoryActivity.java
@@ -3,14 +3,22 @@ package ro.mds.note.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import ro.mds.note.R;
 
 public class HistoryActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
     }
+
 }
--- app/src/main/java/ro/mds/note/activity/MainActivity.java
+++ app/src/main/java/ro/mds/note/activity/MainActivity.java
@@ -1,39 +1,100 @@
-package ro.mds.note.activity;
+package ro.mds.note;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.GestureDetector;
+import android.gesture.Gesture;
+import static android.view.GestureDetector.*;
 
-import ro.mds.note.R;
+public class MainActivity extends AppCompatActivity implements OnGestureListener,OnDoubleTapListener{
 
-public class MainActivity extends AppCompatActivity {
+
+    private static Button button_sbm;
+    private static ListView list_view;
+    private static String[] Names=new String[]{"Tom","Mark","john","jack"};
+    private static TextView textView;
+    private static GestureDetectorCompat gestureDetect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=(TextView)findViewById(R.id.textView);
+        gestureDetect=new GestureDetectorCompat(this,this);
+        gestureDetect.setOnDoubleTapListener(this);
+    }
 
-        Button takeNoteBtn = findViewById(R.id.takeNoteBtn);
-        Button historyBtn = findViewById(R.id.historyBtn);
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetect.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
 
-        takeNoteBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent note = new Intent(MainActivity.this, NoteActivity.class);
-                MainActivity.this.startActivity(note);
-            }
-        });
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        textView.setText("onSingleTapConfirmed "+e.toString());
+        return false;
+    }
 
-        historyBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent history = new Intent(MainActivity.this, HistoryActivity.class);
-                MainActivity.this.startActivity(history);
-            }
-        });
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        textView.setText("onDoubleTap "+e.toString() );
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        textView.setText("onDoubleTapEvent "+e.toString() );
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e)
+    {textView.setText("onDown " +e.toString());
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        textView.setText("onShowPress( " +e.toString());
 
     }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        textView.setText("onSingleTapUp " +e.toString());
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        textView.setText( "onScroll  "+e1.toString()+e2.toString());
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        textView.setText("onLongPress" +e.toString());
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        textView.setText( "onFling"+e1.toString()+e2.toString());
+        return false;
+    }
+
+
 }
